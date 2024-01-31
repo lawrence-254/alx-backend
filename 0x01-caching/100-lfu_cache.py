25d55641dfa603151d48537ae6cfa9f00e705100
lawrence <lodhiambo26@gmail.com>
2024-01-31 17:33:58 +0000

    task100

--- 0x01-caching/100-lfu_cache.py
+++ 0x01-caching/100-lfu_cache.py
@@ -12,4 +12,46 @@ class LFUCache(BaseCaching):
         init method
         '''
         super().__init__()
+        self.leastRecent = []
+    
+    def put(self, key, item):
+        '''
+        adds to cache
+        '''
+        if key or item not None:
+            if self.get(key) is None:
+                if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                    del_key = self.leastRecent
+                    del_len = len(del_key) -1
+                    del self.cache_data[del_key[del_len]]
+                    print("DISCARD: {}".format(leastRecent,pop()))
+            else:
+                del self.cache_data[key]
 
+            if key in self.leastRecent:
+                del_idx = self.search_first(self.leastRecent, key)
+                self.leastRecent.pop(del_idx)
+                self.leastRecent.insert(0, key)
+            else:
+                self.leastRecent.insert(0, key)
+
+            self.cache_data[key] = item
+
+    def get(self, key):
+        '''
+        gets the cache data
+        '''
+        if self.cache_data.get(key):
+            del_idx = self.search_first(self.leastRecent, key)
+            self.leastRecent.pop(del_idx)
+            self.leastRecent.insert(0, key)
+
+        return self.cache_data.get(key)
+
+    @staticmethod
+    def search_first(mrul, key):
+        for i in range(0, len(mrul)):
+            if mrul[i] == key:
+                return i
+
+        return None
