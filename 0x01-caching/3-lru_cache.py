a00de551a6b2fd007d827359c68b036820d87245
lawrence <lodhiambo26@gmail.com>
2024-01-31 09:45:08 +0000

    task3

---
+++ 0x01-caching/3-lru_cache.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+'''
+lru module
+'''
+from base_caching import BaseCaching
+
+
+class LRUCache(BaseCaching):
+    '''
+    a class LRUCache that inherits from BaseCaching and is a caching system
+    '''
+    def __init__(self):
+        super().__init__()
+        self.leastrecentlyused = []
+
+    def put(self, key, item):
+        '''
+        adds item to cache
+        '''
+        if key is None or item is None:
+            return
+
+        if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+            recent = self.leastrecentlyused.pop(0)
+            del self.cache_data[recent]
+            print("DISCARD: {recent}")
+
+        self.cache_data[key] = item
+        self.leastrecentlyused.append(key)
+
+
+    def get(self, key):
+        '''
+        fetch cache items
+        '''
+        if key is None or key not in self.cache_data:
+            return None
+
+        self.leastrecentlyused.remove(key)
+        self.leastrecentlyused.append(key)
+
+        return self.cache_data[key]
