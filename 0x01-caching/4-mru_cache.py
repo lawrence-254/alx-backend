cc4c0df99bf34a622f061c70bd22d24bfed7b199
lawrence <lodhiambo26@gmail.com>
2024-01-31 10:00:30 +0000

    task4

---
+++ 0x01-caching/4-mru_cache.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+""" MRUCache module """
+from base_caching import BaseCaching
+
+
+class MRUCache(BaseCaching):
+    '''
+    class MRUCache that inherits from BaseCaching and is a caching system
+    '''
+    def __init__(self):
+        '''
+        init
+        '''
+        super().__init__()
+
+    def put(self, key, item):
+        '''
+        adds to cache
+        '''
+        if key is None or item is None:
+            return
+
+        if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+            most_recently_used = list(self.cache_data.keys())[-1]
+            del self.cache_data[most_recently_used]
+            print("DISCARD:", most_recently_used)
+
+        self.cache_data[key] = item
+
+    def get(self, key):
+        '''
+        fetches data from the cache
+        '''
+        if key is None or key not in self.cache_data:
+            return
+
+        self.cache_data[key] = self.cache_data.pop(key)
+        return self.cache_data.pop(key)
