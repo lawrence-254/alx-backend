d0ae01268a00de35eae43a942f2e6e7e0cbf4e1c
lawrence <lodhiambo26@gmail.com>
2024-02-02 09:10:32 +0000

    redo3

--- 0x00-pagination/3-hypermedia_del_pagination.py
+++ 0x00-pagination/3-hypermedia_del_pagination.py
@@ -45,24 +45,18 @@ class Server:
         the dataset, the user does not miss items from dataset when changing
         page.
         '''
-        data = self.indexed_dataset()
-        assert index is not None and index >= 0 and index <= max(data.keys())
-        page_data = []
-        data_count = 0
-        next_index = None
-        start = index if index else 0
-        for i, item in data.items():
-            if i >= start and data_count < page_size:
-                page_data.append(item)
-                data_count += 1
-                continue
-            if data_count == page_size:
-                next_index = i
-                break
-        page_info = {
+        assert isinstance(index, int) and 0 <= index < len(
+                self.__indexed_dataset)
+        assert isinstance(page_size, int) and page_size > 0
+
+        data = [self.__indexed_dataset[i] for i in range(
+            index, index + page_size)]
+        next_index = index + page_size if index + page_size < len(
+                self.__indexed_dataset) else None
+
+        return {
             'index': index,
-            'next_index': next_index,
-            'page_size': len(page_data),
-            'data': page_data,
+            'data': data,
+            'page_size': page_size,
+            'next_index': next_index
         }
-        return page_info
