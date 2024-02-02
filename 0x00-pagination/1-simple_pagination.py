8779e64fbe24343d48a5c21455da003e6e144776
lawrence <lodhiambo26@gmail.com>
2024-02-02 08:42:16 +0000

    redo 1

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -48,10 +48,15 @@ class Server:
         takes two integer arguments page with default value 1
         and page_size with default value 10.
         '''
-        assert type(page) == int and type(page_size) == int
-        assert page > o and page_size > 0
-        start, end = index_range(page, page_size)
-        data = self.dataset()
-        if start > len(data):
+        assert isinstance(page, int) and page > 0
+        assert isinstance(page_size, int) and page_size > 0
+
+        dataset = self.dataset()
+        total_rows = len(dataset)
+
+        start_index = (page - 1) * page_size
+        end_index = start_index + page_size
+
+        if start_index >= total_rows or page <= 0:
             return []
-        return data[start:end]
+        return dataset[start_index:end_index]
