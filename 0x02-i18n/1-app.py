36f3116190af60135f1b4fea98bdad4a89a84766
lawrence <lodhiambo26@gmail.com>
2024-02-06 15:57:09 +0000

    1

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -2,18 +2,29 @@
 '''
 a basic Flask app
 '''
+from flask_babel import Babel
 from flask import Flask, render_template
 
+class Config:
+    '''
+    configures babel languages
+    '''
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
 app = Flask(__name__)
+app.config.from_object(Config)
 app.url_map.strict_slashes = False
+babel = Babel(app)
 
 
 @app.route('/')
-def index():
+def index() -> str:
     '''
     route for index.html
     '''
-    return render_template('0-index.html')
+    return render_template('1-index.html')
 
 
 if __name__ == '__main__':
