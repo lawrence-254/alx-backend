18a6bbc196ed74309acd949da109fba8799d65cb
lawrence <lodhiambo26@gmail.com>
2024-02-06 16:24:12 +0000

    2

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -3,7 +3,7 @@
 a basic Flask app
 '''
 from flask_babel import Babel
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 
 class Config:
@@ -20,6 +20,13 @@ app.config.from_object(Config)
 app.url_map.strict_slashes = False
 babel = Babel(app)
 
+
+@babel.localeselector
+def get_locale() -> str:
+    '''
+    fetches for locale of the user
+    '''
+    return request.accept_languages.best_match(app.config["LANGUAGES"])
 
 @app.route('/')
 def index() -> str:
